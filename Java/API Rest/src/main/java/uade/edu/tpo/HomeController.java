1660a3d3fff73625933b5c95fe986524f5c25792
MelodRAMAtick <melodramatick@users.noreply.github.com>
2020-06-14 19:47:58 -0300

    Ajuste de back end
    Actualizo POM con las dependencias de Jackson y los cambios que hice en el HC. Ahora pusheo lo que hice en el front end

--- Java/API Rest/src/main/java/uade/edu/tpo/HomeController.java
+++ Java/API Rest/src/main/java/uade/edu/tpo/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import controlador.Controlador;
+import modelo.Usuario;
 
 /**
  * Handles requests for the application home page.
@@ -22,6 +29,9 @@ public class HomeController {
 	private String nomSession, apeSession, fdnSession;
 	private int dniSession;
 	
+	private ObjectMapper om= new ObjectMapper();
+	private String res = null;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -41,20 +51,22 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value = "/verificarLogin", method = RequestMethod.GET, produces = {"application/json"})
-	public @ResponseBody<json> boolean verificarLogin(@RequestParam(value="usuario", required = true) String usuario,  
+	@RequestMapping(value = "/verificarLogin", method = RequestMethod.POST, produces = {"application/json"})
+	public @ResponseBody<json> String verificarLogin(@RequestParam(value="usuario", required = true) String usuario,  
 			@RequestParam(value="password", required = true) String password) throws JsonProcessingException{
 		boolean b = false;
+		System.out.println(usuario+' '+password);
 		Usuario usr= Controlador.getInstancia().verficarLogin(usuario, password);
 		if(usr != null) {
 			b= true;
+			res = om.writeValueAsString(usr);
 			this.nomSession= usr.getNombre();
 			this.apeSession= usr.getApellido();
 			this.fdnSession= usr.getFechaDeNacimiento();
 			this.dniSession = usr.getDni();
 		}
-//		System.out.println(this.usuario);
-		return b;
+		System.out.println(b);
+		return res;
 	}
 
 	
